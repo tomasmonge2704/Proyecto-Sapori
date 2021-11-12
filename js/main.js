5e4ba5656e369fc931ccb4d71b0ef6bb59c72881
tomasmonge2704 <tomas.monge.vidal@gmail.com>
2021-11-12 17:27:39 -0300

    new testimonial

--- js/main.js
+++ js/main.js
@@ -302,3 +302,16 @@ function isValidEmail(email) {
 };
 
 
+$(document).ready(function(){
+	$("#testimonial-slider").owlCarousel({
+		items:2,
+		itemsDesktop:[1000,2],
+		itemsDesktopSmall:[990,2],
+		itemsTablet:[768,1],
+		pagination:true,
+		navigation:false,
+		navigationText:["",""],
+		slideSpeed:1000,
+		autoPlay:true
+	});
+});
